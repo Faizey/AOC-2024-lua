46e307ec02f0f7c955690456eb59ac8ca88ab958
Faizey <shaun_miah08@hotmail.co.uk>
2024-12-03 12:15:41 +0000

    AOC lua day-1

--- day-1/solutions.lua
+++ day-1/solutions.lua
@@ -1,13 +1,50 @@
 local module = {}
 
 function module.solution_1(input)
-    print(input)
-    return "solution 1"
+    local answer = 0
+    local left_col = {}
+    local right_col = {}
+
+    for num1, num2 in string.gmatch(input, "(%d+)%s+(%d+)") do
+        table.insert(left_col, num1)
+        table.insert(right_col, num2)
+    end
+
+    table.sort(left_col)
+    table.sort(right_col)
+
+    for i = 1, #left_col do
+        local distance = math.abs(left_col[i] - right_col[i])
+        answer = answer + distance
+    end
+
+    return answer
 end
 
 function module.solution_2(input)
-    print(input)
-    return "solution 2"
+    local answer = 0
+    local left_col = {}
+    local right_col = {}
+
+    for num1, num2 in string.gmatch(input, "(%d+)%s+(%d+)") do
+        table.insert(left_col, num1)
+        table.insert(right_col, num2)
+    end
+
+    for i = 1, #left_col do
+        local current_number = left_col[i];
+        local same_num_count = 0;
+
+        for y = 1, #right_col do
+            if current_number == right_col[y] then
+                same_num_count = same_num_count + 1
+            end
+        end
+
+        answer = answer + (current_number * same_num_count)
+    end
+
+    return answer
 end
 
 return module
